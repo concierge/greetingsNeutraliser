d59cef218c6cf4ce664e131e2b07c8c6ccc7ed00
Dion Woolley <woolley.dion@gmail.com>
2017-04-25 22:06:41 +1200

    Implementation for greetings neutraliser

---
+++ greetingsNeutraliser.js
@@ -0,0 +1,17 @@
+const nonNeturalTerms = ["guys", "boys", "lads", "dudes", "men", "gals", "girls", "women", "ladies"],
+    neturalTerms = ["team", "y’all", "folks", "everyone", "group"];
+
+
+exports.match = (event, commandPrefix) => {
+    if (new RegExp(nonNeturalTerms.join("|"), "i").test(event.body)) {
+        return true;
+    }
+    return false;
+}
+
+exports.run = (api, event) => {
+    let terms = neturalTerms,
+        index = Math.floor(Math.random() * terms.length),
+        term = terms.splice(index, 1);
+    api.sendMessage("I see you used a non gender netural term in your previous message\nYou could replace it with \"" + term + "\" or any of the following: " + terms.join(", "), event.thread_id);
+}
